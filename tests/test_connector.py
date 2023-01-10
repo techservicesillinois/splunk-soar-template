654b71ce4460c46ee2087753c2e03017bbc8166f
Edward Delaporte <edward@delaporte.us>
2023-01-10 21:23:38 +0000

    Fixup - remove unused name

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -1,26 +1,22 @@
 import json
-from urllib.error import HTTPError
 
-from vcr import cassette
-
-from requests.exceptions import HTTPError
-
-from phTDX.tdx_connector import TdxConnector
+from phIllinois_app import AppConnector
 
 APP_ID = "tacosalad"
 
-def test_connectivity(cassette, connector: TdxConnector):
+
+def test_connectivity(cassette, connector: AppConnector):
     in_json = {
             "appid": APP_ID,
             "identifier": "test_connectivity",
-            "parameters": [{}], # TODO: Submit an issue asking to allow [] here.
+            "parameters": [{}],
     }
 
     result = json.loads(connector._handle_action(json.dumps(in_json), None))
     assert result[0]["message"] == "Active connection"
 
 
-def test_failed_connectivity(cassette, connector: TdxConnector):
+def test_failed_connectivity(cassette, connector: AppConnector):
     in_json = {
             "appid": APP_ID,
             "identifier": "test_connectivity",
