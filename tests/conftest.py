f566a5afdf4413aa0faa98a8a8f335c8be16e4e3
Edward Delaporte <edward@delaporte.us>
2023-01-10 14:53:34 +0000

    Remove non-reusable test code

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,11 +18,6 @@ CASSETTE_USERNAME = "FAKE_USERNAME"
 CASSETTE_PASSWORD = "FAKE_PASSWORD"
 CASSETTE_NETID = 'thor2'
 CASSETTE_ENDPOINT = "help.uillinois.edu"
-CASSETTE_ACCOUNT_NAME = "None/Not Found"  # TODO: Pull from config as part of issue #13
-CASSETTE_ORG_NAME = "Marvel U"
-CASSETTE_TIMEZONE = "0000"
-CASSETTE_LOG_LEVEL = "DEBUG"
-APPID = 66  # APPID and URL are also CASSETTE but need short names
 URL = f"https://{CASSETTE_ENDPOINT}"
 
 # To record, `export VCR_RECORD=True`
@@ -32,7 +27,7 @@ VCR_RECORD = "VCR_RECORD" in os.environ
 class CleanYAMLSerializer:
     def serialize(cassette: dict):
         for interaction in cassette['interactions']:
-            clean_token(interaction)
+            clean_jwt_token(interaction)
             clean_search(interaction)
             clean_new_ticket(interaction)
             clean_people_lookup(interaction)
@@ -42,8 +37,8 @@ class CleanYAMLSerializer:
         return yamlserializer.deserialize(cassette)
 
 
-def clean_token(interaction: dict):
-    uri = f"{URL}/SBTDWebApi/api/auth"
+def clean_jwt_token(interaction: dict):
+    uri = f"{URL}/api/auth"  # TODO: Adjust URI to match auth URL in use.
     if interaction['request']['uri'] != uri:
         return
 
@@ -56,104 +51,26 @@ def clean_token(interaction: dict):
     response['body']['string'] = token
 
 
-def clean_search(interaction: dict):
-    uri = f"{URL}/SBTDWebApi/api/accounts/search"
-    if interaction['request']['uri'] != uri:
-        return
-
-    body = json.loads(interaction['response']['body']['string'])
-    result = {}
-    for item in body:
-        if item['Name'] == CASSETTE_ACCOUNT_NAME:
-            result = item
-    body = [result]
-
-    interaction['response']['body']['string'] = json.dumps(body)
-
-
-def clean_new_ticket(interaction: dict):
-    id = 564073
-    uri = f"{URL}/SBTDWebApi/api/{APPID}/tickets/?EnableNotifyReviewer=False" + \
-        "&NotifyRequestor=False&NotifyResponsible=False" + \
-        "&AllowRequestorCreation=False"
-
-    if interaction['request']['uri'] != uri:
-        return
-
-    body = json.loads(interaction['response']['body']['string'])
-    body['Uri'] = body['Uri'].replace(str(body['ID']), str(id))
-    body['ID'] = id
-    body['RequestorEmail'] = 'nobody@example.com'
-    body['RequestorName'] = 'Jane Foster'
-    body['RequestorFirstName'] = 'Jane'
-    body['RequestorLastName'] = 'Foster'
-    body['RequestorPhone'] = None
-
-    body['Notify'][0]['Name'] = 'Jane Foster'
-    body['Notify'][0]['Value'] = 'nobody@example.com'
-    interaction['response']['body']['string'] = json.dumps(body)
-
-
-def clean_people_lookup(interaction: dict):
-    # TODO: Switch the NetID here based on ENV settings and record mode
-    netid = os.environ.get('TDX_NETID', CASSETTE_NETID)
-    uri = "%s/SBTDWebApi/api/people/lookup?searchText=%s&maxResults=1"
-
-    if interaction['request']['uri'] != uri % (URL, netid):
-        return
-
-    interaction['request']['uri'] = uri % (URL, 'thor2')
-    body = json.loads(interaction['response']['body']['string'])
-
-    body[0]['Salutation'] = 'Doctor'
-    body[0]['FirstName'] = 'Jane'
-    body[0]['LastName'] = 'Foster'
-    body[0]['MiddleName'] = None
-    body[0]['FullName'] = 'Jane Foster'
-    body[0]['Nickname'] = 'The Mighty Thor'
-
-    body[0]['HomePhone'] = None
-    body[0]['PrimaryPhone'] = None
-    body[0]['WorkPhone'] = None
-    body[0]['OtherPhone'] = None
-    body[0]['MobilePhone'] = None
-
-    body[0]['PrimaryEmail'] = 'nobody@example.com'
-    body[0]['AlternateEmail'] = 'nobody@example.com'
-    body[0]['AlertEmail'] = 'nobody@example.com'
-
-    interaction['response']['body']['string'] = json.dumps(body)
-
-
 @pytest.fixture
-def connector(monkeypatch) -> TdxConnector:
-    conn = TdxConnector()
+def connector(monkeypatch) -> AppConnector:
+    conn = AppConnector()
     if not VCR_RECORD:  # Always use cassette values when using cassette
-        #  TODO: Lots more configs!
         conn.config = {
             "username": CASSETTE_USERNAME,
             "password": CASSETTE_PASSWORD,
             "endpoint": CASSETTE_ENDPOINT,
-            "appid": APPID,
-            "orgname": CASSETTE_ORG_NAME,
-            "timezone": CASSETTE_TIMEZONE,
-            "loglevel": CASSETTE_LOG_LEVEL,
-            "sandbox": True,
         }
-        os.environ.pop('TDX_NETID', None)
     else:  # User environment values
         env_keys = ['username','password','netid',
-                    'endpoint','appid', 'orgname',
-                    'timezone', 'loglevel']
+                    'endpoint']
 
         for key in env_keys:
-            env_key = f"TDX_{key.upper()}"
+            # TODO: Change APP_ on next line to avoid key conflicts
+            env_key = f"APP_{key.upper()}"  
             conn.config[key] = os.environ.get(env_key, None)
             if not conn.config[key]:
                 raise ValueError(f'{env_key} unset or empty with record mode')
         
-        conn.config['sandbox'] = True  # Always True - no testing in production.
-
     conn.logger.setLevel(logging.INFO)
     return conn
 
