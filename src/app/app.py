8706fd6533d4a3aa0974f55ba6030c88a7d329db
Zach Carrington <zdc@illinois.edu>
2023-06-15 21:04:56 +0000

    Update app.py

--- src/app/app.py
+++ src/app/app.py
@@ -12,14 +12,13 @@ import requests
 
 from phtoolbox.app.base_connector import NiceBaseConnector, handle
 
-__version__ = 'GITHUB_TAG'
-__git_hash__ = 'GITHUB_SHA'
-__build_time__ = 'BUILD_TIME'
+__version__ = "GITHUB_TAG"
+__git_hash__ = "GITHUB_SHA"
+__build_time__ = "BUILD_TIME"
 
 
 class AppConnector(NiceBaseConnector):
-
-    @handle('test_connectivity', 'test_robots_txt')
+    @handle("test_connectivity", "test_robots_txt")
     def _handle_test_connectivity(self, param):
         action_result = self.add_action_result(ActionResult(dict(param)))
 
@@ -30,14 +29,13 @@ class AppConnector(NiceBaseConnector):
         )
         if response.status_code != 200:
             self.save_progress("Test Connectivity Failed.")
-            return action_result.set_status(phantom.APP_ERROR,
-                                            "Failed connection")
+            return action_result.set_status(phantom.APP_ERROR, "Failed connection")
 
         # Return success
         self.save_progress(
-            f"Test Connectivity Passed: {__version__} ({__git_hash__[0:7]})")
-        return action_result.set_status(phantom.APP_SUCCESS,
-                                        "Active connection")
+            f"Test Connectivity Passed: {__version__} ({__git_hash__[0:7]})"
+        )
+        return action_result.set_status(phantom.APP_SUCCESS, "Active connection")
 
     def initialize(self):
         ret = super(AppConnector, self).initialize()
@@ -54,9 +52,9 @@ class AppConnector(NiceBaseConnector):
         optional_config_name = config.get('optional_config_name')
         """
 
-        self._endpoint = config['endpoint']
-        self._auth = (config['username'], config['password'])
+        self._endpoint = config["endpoint"]
+        self._auth = (config["username"], config["password"])
         self._baseurl = f"https://{self._endpoint}"
-        self._username = config['username']
+        self._username = config["username"]
 
         return ret
