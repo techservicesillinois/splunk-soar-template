2054fedada72d1e88bce912962c19d8df754629c
danielgfine <99198224+danielgfine@users.noreply.github.com>
2024-10-14 15:50:25 -0500

    Add guidance on generating a new uuid (#72)
    * Add guidance on generating a new uuid
    * Update to not deploy because this is a template
    
    ---------
    
    Co-authored-by: Daniel Fine <dfine2@illinois.edu>
    Co-authored-by: Michelle Pitcel <mpitcel2@illinois.edu>
    Co-authored-by: Zach Carrington <zdc@illinois.edu>

--- config.mk
+++ config.mk
@@ -1,3 +1,10 @@
+# Replace both UUIDs below with brand new values
+# Windows users can use the new command "New-Guid"
+# Mac users can use "uuidgen" 
+# Linux users can use  "uuidgen -r"
+
+# WARNING the command "uuid" results in collisions and will not work
+
 PROD_APP_ID:=fc618bee-352f-461a-95b5-bc0a2395302a
 PROD_APP_NAME:=Template DO NOT USE
 TEST_APP_ID:=5e842053-7364-4a90-a275-2bd838458fef
