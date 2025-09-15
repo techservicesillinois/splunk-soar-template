8b7f3b2a9d2b4ba82335176ec842577142ddd76c
Edward Delaporte <delaport@illinois.edu>
2025-09-15 10:17:37 -0500

    Add workflow dispatch option to main CI/CD (#79)

--- tests/test_python_version.py
+++ tests/test_python_version.py
@@ -1,10 +1,9 @@
 import sys
 
-SOAR_PYTHON_VERSION = (3, 9, 9)
+SOAR_PYTHON_VERSION = (3, 13)
 
 
 def test_python_version():
     """ Ensure python version in use matches SOAR python version. """
     assert sys.version_info.major == SOAR_PYTHON_VERSION[0]
     assert sys.version_info.minor == SOAR_PYTHON_VERSION[1]
-    assert sys.version_info.micro == SOAR_PYTHON_VERSION[2]
