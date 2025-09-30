659c8a9b1b0014e0c80f38dec8105055d765815b
Edward <delaport@illinois.edu>
2025-09-30 14:05:17 -0500

    WIP - Use NiceBaseConnector for test_connectivity

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -1,27 +1,27 @@
 import json
 
-from app import AppConnector
+from phtoolbox.app.base_connector import NiceBaseConnector
 
 APP_ID = "tacosalad"
 
 
-def test_connectivity(cassette, connector: AppConnector):
+def test_connectivity(cassette, connector: NiceBaseConnector):
     in_json = {
             "appid": APP_ID,
             "identifier": "test_connectivity",
             "parameters": [{}],
     }
 
-    result = json.loads(connector._handle_action(json.dumps(in_json), None))
+    result = json.loads(connector.nice_handle_action(json.dumps(in_json)))
     assert result[0]["message"] == "Active connection"
 
 
-def test_failed_connectivity(cassette, connector: AppConnector):
+def test_failed_connectivity(cassette, connector: NiceBaseConnector):
     in_json = {
             "appid": APP_ID,
             "identifier": "test_connectivity",
             "parameters": [{}],
     }
 
-    result = json.loads(connector._handle_action(json.dumps(in_json), None))
+    result = json.loads(connector.nice_handle_action(json.dumps(in_json)))
     assert result[0]["message"] == "Failed connection"
