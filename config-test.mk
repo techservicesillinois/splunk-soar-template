4dd2eff9864261082b504823113a6c619400b11c
David D. Riddle <ddriddle@users.noreply.github.com>
2025-10-21 11:33:43 -0500

    Remove build-test target (#103)
    To build a test app set the var SOAR_TEST_APP.
    
    Closes #101

--- config-test.mk
+++ config-test.mk
@@ -5,5 +5,5 @@
 
 # WARNING the command "uuid" results in collisions and will not work
 
-APP_ID:=fc618bee-352f-461a-95b5-bc0a2395302a
-APP_NAME:=Template DO NOT USE
+APP_ID:=5e842053-7364-4a90-a275-2bd838458fef
+APP_NAME:=Test Template DO NOT USE
